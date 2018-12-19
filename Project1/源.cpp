3615247cf84e9bddac3aaa9352c98aa213208b65
旺斌 张 <1521384804@qq.com>
2018-12-19 13:06:25 +0800

    a

---
+++ Project1/源.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double ctemp, ftemp;
+	cin >> ctemp;
+	ftemp = ctemp * 1.8 + 32;
+	cout << ftemp;
+	while (1)
+	{
+
+	}
+	return 0;
+	
+
+}
